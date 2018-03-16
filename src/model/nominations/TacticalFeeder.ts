b048abf08732957cd5fedaf14386d0ec1afc9975
Tee Hee <zheliznjak@gmail.com>
2018-03-16 22:24:17 +0200

    asdf

--- src/model/nominations/TacticalFeeder.ts
+++ src/model/nominations/TacticalFeeder.ts
@@ -1,6 +1,7 @@
 import Nomination from "../Nomination";
 import { DotaParser } from "../../services/DotaParser";
 import Pair from "../Pair";
+import { PlayerJson } from "../../dota-api/DotaJsonTypings";
 
 export class TacticalFeeder extends Nomination {
     constructor(protected points: Array<Pair<string, number | string>> = []) {
@@ -12,8 +13,8 @@ export class TacticalFeeder extends Nomination {
 
     protected scorePoint(match, player_slot): number {
         if (!!match) {
-            const player = DotaParser.getPlayerInfo(match, player_slot);
-            return player && player.deaths && player.deaths > 10 && player.win ? 1 : 0;
+            const player: PlayerJson = DotaParser.getPlayerInfo(match, player_slot);
+            return player && player.deaths && player.deaths > 10 && player.win === 1 ? 1 : 0;
         }
         return 0;
     }
