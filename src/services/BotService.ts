ea36df97074bbdfe135d4986b839380e103400c8
SerhiyZheliznjak <zheliznjak@gmail.com>
2018-03-27 18:15:33 +0300

    fixed broken reply

--- src/services/BotService.ts
+++ src/services/BotService.ts
@@ -258,7 +258,7 @@ export default class BotService {
                                 const place = index + 1;
                                 msgText += place + ') ' + profileMap.get(tr.account_id) + ':\t' + tr.nomination.getScoreText() + '\n';
                             });
-                            msg.reply(this.getRichEmbed(firstNomination.getName(), msgText, undefined, '#Тайтаке.'));
+                            this.chanel.send('', this.getRichEmbed(firstNomination.getName(), msgText, undefined, '#Тайтаке.'));
                         });
                 });
             } else {
