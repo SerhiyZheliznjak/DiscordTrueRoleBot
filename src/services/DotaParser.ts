24a690eca7cbd31993331dbb32d49a7da30cec81
SerhiyZheliznjak <zheliznjak@gmail.com>
2018-06-05 12:10:40 +0300

    fixed dota parser rare issues

--- src/services/DotaParser.ts
+++ src/services/DotaParser.ts
@@ -2,15 +2,21 @@ import { MatchJson, PlayerJson } from "../dota-api/DotaJsonTypings";
 
 export class DotaParser {
     public static getPlayerInfo(match: MatchJson, player_slot: number): PlayerJson {
-        return !!match ? match.players.find(player => player.player_slot === player_slot) : undefined;
+        if (match && match.players) {
+            return match.players.find(player => player.player_slot === player_slot);
+        }
+        return undefined;
     }
 
     public static getObjectives(match: MatchJson) {
         return !!match ? match.objectives : undefined;
     }
 
-    public static getPlayerSlot(match: MatchJson, account_id: number) {
-        const player = !!match ? match.players.find(p => p.account_id === account_id) : null;
+    public static getPlayerSlot(match: MatchJson, account_id: number): number {
+        let player;
+        if (match && match.players) {
+            player = match.players.find(p => p.account_id === account_id);
+        }
         if (player) {
             return player.player_slot;
         }
