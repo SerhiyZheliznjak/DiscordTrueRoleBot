e9fd145adecd99a690d0c02c0c2a2ae39fd87261
Serhiy Zheliznyak <zheliznjak@gmail.com>
2018-08-03 21:34:04 +0300

    huy

--- src/model/commands/TopTeams.ts
+++ src/model/commands/TopTeams.ts
@@ -3,11 +3,13 @@ import { Message } from "discord.js";
 import { DiscordUtils } from "../../utils/DiscordUtils";
 
 export class TopTeams extends CommandBase {
+    private defaultN = 5;
+
     public process(msg: Message): void {
         if (!this.isLocked(msg)) {
             this.dataStore.getTeams().subscribe(teams => {
                 const digits = this.getArgs(msg.content.toLowerCase()).find(arg => /\d+/.test(arg));
-                const n = !digits ? 10 : +digits.match(/\d+/)[0];
+                const n = !digits ? this.defaultN : +digits.match(/\d+/)[0];
                 const topTeams = teams.slice(0, n);
                 const maxNameLength = Math.max(...(topTeams.map(t => t.name.length)));
                 let nameText = 'Команда';
@@ -19,7 +21,7 @@ export class TopTeams extends CommandBase {
                 const message = topTeams.reduce((msg, team) => {
                     const winrate = DiscordUtils.getPercentString(Math.round(team.wins / (team.losses + team.wins) * 10000) / 100);
                     return msg + DiscordUtils.fillWithSpaces(String(team.name), nameText.length) + ' | '
-                        +DiscordUtils.fillWithSpaces(String(winrate), winrateText.length) + ' | ' + team.losses + team.wins + '\n';
+                        + DiscordUtils.fillWithSpaces(String(winrate), winrateText.length) + ' | ' + team.losses + team.wins + '\n';
                 }, '```' + nameText + ' | ' + winrateText + ' | ' + sumText + '\n');
                 msg.channel.send(message + '```');
             });
@@ -27,6 +29,6 @@ export class TopTeams extends CommandBase {
     }
 
     public helpText(): string {
-        return 'Повертає топ N професійних команд, N = 10 за замовчуванням';
+        return 'Повертає топ N професійних команд, N = ' + this.defaultN + ' за замовчуванням';
     }
 }
