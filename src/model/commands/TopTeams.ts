56b895334b8cb1a87a9d5f0f7538e5185e348f49
Serhiy Zheliznyak <zheliznjak@gmail.com>
2018-08-03 21:31:25 +0300

    fit and finish

--- src/model/commands/TopTeams.ts
+++ src/model/commands/TopTeams.ts
@@ -10,11 +10,17 @@ export class TopTeams extends CommandBase {
                 const n = !digits ? 10 : +digits.match(/\d+/)[0];
                 const topTeams = teams.slice(0, n);
                 const maxNameLength = Math.max(...(topTeams.map(t => t.name.length)));
+                let nameText = 'Команда';
+                const winrateText = 'Вінрейт';
+                const sumText = 'Матчів';
+                if (maxNameLength > nameText.length) {
+                    nameText = DiscordUtils.fillWithSpaces(nameText, maxNameLength);
+                }
                 const message = topTeams.reduce((msg, team) => {
                     const winrate = DiscordUtils.getPercentString(Math.round(team.wins / (team.losses + team.wins) * 10000) / 100);
-                    return msg + DiscordUtils.fillWithSpaces(String(team.name), Math.max(maxNameLength, 7)) + ' | '
-                        +DiscordUtils.fillWithSpaces(String(winrate), 7) + ' | ' + team.losses + team.wins + '\n';
-                }, '```Команда | Вінрейт | Матчів\n');
+                    return msg + DiscordUtils.fillWithSpaces(String(team.name), nameText.length) + ' | '
+                        +DiscordUtils.fillWithSpaces(String(winrate), winrateText.length) + ' | ' + team.losses + team.wins + '\n';
+                }, '```' + nameText + ' | ' + winrateText + ' | ' + sumText + '\n');
                 msg.channel.send(message + '```');
             });
         }
