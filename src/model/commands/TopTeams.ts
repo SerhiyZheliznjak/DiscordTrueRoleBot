0005a0ab9c8dc316305346d06522c76b4a8c9a61
SerhiyZheliznjak <sez@csia.net>
2018-10-11 10:46:26 +0300

    added failsafe when API doesn't provide no team name or tag

--- src/model/commands/TopTeams.ts
+++ src/model/commands/TopTeams.ts
@@ -17,7 +17,7 @@ export class TopTeams extends CommandBase {
                 const topTeams = teams.slice(0, numberOfTeams);
                 const maxNameLength = Math.max(...(topTeams.map((t, index) => {
                     const placeText = this.getPlaceText(index);
-                    return t.name.length + placeText.length;
+                    return this.getTeamName(t).length + placeText.length;
                 })));
 
                 if (maxNameLength > this.nameText.length) {
@@ -26,7 +26,7 @@ export class TopTeams extends CommandBase {
                 const message = topTeams.reduce((text, team) => {
                     const winrate = DiscordUtils.getPercentString(Math.round(team.wins / (team.losses + team.wins) * 10000) / 100);
                     const gamesPlayed = team.losses + team.wins;
-                    return text + (this.getPlaceText(topTeams.indexOf(team)) + team.name).padEnd(this.nameText.length) + ' | '
+                    return text + (this.getPlaceText(topTeams.indexOf(team)) + this.getTeamName(team)).padEnd(this.nameText.length) + ' | '
                      + String(winrate).padEnd(this.winrateText.length) + ' | '
                      + gamesPlayed + '\n';
                 }, '');
@@ -76,5 +76,9 @@ export class TopTeams extends CommandBase {
 
     private getPlaceText(index: number): string {
         return index + 1 + '. ';
+    }
+
+    private getTeamName(team: TeamJson): string {
+        return !!team.name ? team.name : !!team.tag ? team.tag : 'API issue!';
     }
 }
